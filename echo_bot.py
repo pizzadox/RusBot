cfb26161966d751ea3304459a50cca2da4dd6f21
vehametra <vehametra@gmail.com>
2023-01-07 02:04:34 +0300

    Добавлена проверка сообщения определённому шаблону. send message это не print, кучу аргументов не принимает. поправил

--- echo_bot.py
+++ echo_bot.py
@@ -36,13 +36,13 @@ def get_text_messages(message):
     elif message.text == 'Советы':
         bot.send_message(message.from_user.id, 'Подробно про советы по ' + '[ссылке](https://oknarus.com/#services)', parse_mode='Markdown')
 
-    elif re.match(szPtrn, message.text): # размер число1 число2 (3-4х значные
+    elif szPtrn.match( message.text): # размер число1 число2 (3-4х значные
         # разбираем строку , вытаскиваем два числа price = (ширина+высота)*2* цена_бруска + ширина * высота* цена полотна.
         #но надо брать формулу от заказчика
         words = message.text.split()
         a=int(words[1])
         b=int(words[2])
-        bot.send_message(message.from_user.id, 'Расчётная стоимость составит ', a*b + 2*(a+b)) # формула вообще из базы должна браться
+        bot.send_message(message.from_user.id, 'Расчётная стоимость составит: ' +  str(a*b + 2*(a+b)+1) # формула вообще из базы должна браться
 
     elif message.text == 'Знаю размер':
         bot.send_message(message.from_user.id, 'Укажи размеры в поле ввода текста в Миллиметрах ( ширина/высота )', parse_mode='Markdown')
