186b2fb4c36a5bc4dfef71a43efc76b887a00fa3
pizza <mirgorodmi87@gmail.com>
2023-01-07 23:51:41 +0300

    1

--- okno.py
+++ okno.py
@@ -1,9 +1,9 @@
-from echo_bot import height, width
+# from echo_bot import height, width
 
 # Определяем переменные конструкции
 # Следуюие данные берем у клиента в файле echo_bot
-# height = 100
-# width = 200
+height = 100
+width = 200
 rama_profile_h = 6000
 shtapik_profile_h = 6000
 # Определяем фурнитуру для конструкции
@@ -23,7 +23,7 @@ vent_zaglyshka = vent_zaglyshka * 2
 uplotnitel_1 = height * 2 + width * 2 - 328
 uplotnitel_2 = height * 2 + width * 2 - 288
 filling_h = width - 66
-filling_l = hight - 66
+filling_l = height - 66
 window_filling = [filling_l, filling_h]
 # Считаем необходимые материалы
 rama_profile = (rama_l + rama_h) / rama_profile_h
@@ -43,3 +43,4 @@ print(' - Уплонитель ( 521010 ): ', uplotnitel_1, 'мм.')
 print(' - Уплонитель ( 522020 ): ', uplotnitel_2, 'мм.')
 print('- Стекло :')
 print(' - Заполнение ( 6мм ): ', filling_l,'*',filling_h,  'мм')
+print(window_filling)
