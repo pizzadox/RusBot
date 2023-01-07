3088dbd771de4366185c6ffa526cc708d2355d7a
pizza <mirgorodmi87@gmail.com>
2023-01-07 16:35:16 +0300

    1

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ conn = sqlite3.connect('orders.db')
 cur = conn.cursor()
 
 # Создаем таблицы
+    # Таблица в которой лежат ИД/Производитель/Серия
 cur.execute("""CREATE TABLE IF NOT EXISTS constuctions(
    id INT PRIMARY KEY,
    name TEXT,
    product TEXT);
 """)
+    # Таблиа в которой лежат ИД/Название/Материал/Стоимость
 cur.execute("""CREATE TABLE IF NOT EXISTS items(
    id INT PRIMARY KEY,
    name TEXT,
