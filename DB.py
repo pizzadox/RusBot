3951bb85db5f92b6c939846cb70015a35e97dd80
pizza <mirgorodmi87@gmail.com>
2023-01-07 16:01:22 +0300

    1

--- DB.py
+++ DB.py
@@ -1,6 +1,3 @@
-import echo_bot
-import setup
-import
 cur.execute("""CREATE TABLE IF NOT EXISTS constuctions(
    id INT PRIMARY KEY,
    name TEXT,
--- setup.py
+++ setup.py
@@ -2,3 +2,9 @@
 import sqlite3
 conn = sqlite3.connect('orders.db')
 cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS constuctions(
+   id INT PRIMARY KEY,
+   name TEXT,
+   product TEXT);
+""")
+conn.commit()
