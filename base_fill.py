3f9361c5c19e41db5a4093009e5329a5e44b3d93
pizza <mirgorodmi87@gmail.com>
2023-01-07 16:30:07 +0300

    1

--- base_fill.py
+++ base_fill.py
@@ -1,5 +1,7 @@
+# Приложение для внесения в БД данных
 import sqlite3
 conn = sqlite3.connect('orders.db')
 cur = conn.cursor()
-cur.execute("INSERT INTO constuctions VALUES(1, Krauss, KRDW45);")
+cur.execute("""INSERT INTO constuctions(id, name, product) 
+   VALUES(1, 'Krauss', 'KRDW45');""")
 conn.commit()
