5bbe4d47f7dfcc8d7ac1c0aaa13899be9d506b7e
pizza <mirgorodmi87@gmail.com>
2023-01-07 20:54:35 +0300

    1

--- DBase/base_fill.py
+++
@@ -1,7 +0,0 @@
-# Приложение для внесения в БД данных
-#import sqlite3
-#conn = sqlite3.connect('orders.db')
-#cur = conn.cursor()
-#cur.execute("""INSERT INTO constuctions(id, name, product)
-#   VALUES(2, 'Alumark', 'S44');""")
-#conn.commit()
