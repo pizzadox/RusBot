fa82f3a551a63c1b546fc1c0a573d7bc27def4f4
pizza <mirgorodmi87@gmail.com>
2023-01-07 14:06:56 +0300

    lst

--- okno.py
+++ okno.py
@@ -12,24 +12,24 @@ vent_zaglyshka = 1
 # Формула расчета для Конструкции ОКНО глухое
 rama_l = height * 2
 rama_h = width * 2
-shtapik_l = rama_l - 52
-shtapik_h = rama_h - 98
+shtapik_l = width - 52
+shtapik_h = height - 98
 suhar_rama = suhar_rama * 4
 suhar_mini = suhar_mini * 4
 podkladka_pod_steklo = podkladka_pod_steklo * 4
 vent_zaglyshka = vent_zaglyshka * 2
-uplotnitel_1 = rama_l * 2 + rama_h * 2 - 328
-uplotnitel_2 = rama_l * 2 + rama_h * 2 - 288
-filling_h = rama_h - 66
-filling_l = rama_l - 66
+uplotnitel_1 = height * 2 + width * 2 - 328
+uplotnitel_2 = height * 2 + width * 2 - 288
+filling_h = width - 66
+filling_l = height - 66
 # Считаем необходимые материалы
 rama_profile = (rama_l + rama_h) / rama_profile_h
 shtapik_profile = (shtapik_l + shtapik_h) / shtapik_profile_h
 # Выводим расчет
 print('Расчет материалов :')
 print('- Профиль :')
-print(' - Рамный профиль ( 121010 ): ', rama_profile, 'профиля длинной :', rama_profile_h)
-print(' - Штапик ( 125060 ): ', shtapik_profile, 'профиля длинной :', shtapik_profile_h)
+print(' - Рамный профиль ( 121010 ): ', "%.2f" % rama_profile, 'профиля длинной :', rama_profile_h)
+print(' - Штапик ( 125060 ): ', "%.2f" % shtapik_profile, 'профиля длинной :', shtapik_profile_h)
 print('- Комплектующие :')
 print(' - Сухарь угловой ( 721014 ): ', suhar_rama, 'шт.')
 print(' - Сухарь установочный ( 723020 ): ', suhar_mini, 'шт.')
